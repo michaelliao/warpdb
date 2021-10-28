9253cd5d0ecab0938110f76a3d69666438cc4832
Michael Liao <askxuefeng@gmail.com>
2021-10-28 17:21:18 +0800

    fix check-java-version script

--- check_java_version.sh
+++ check_java_version.sh
@@ -2,7 +2,7 @@
 
 JAVA_VERSION=`java -version 2>&1 | awk 'NR==1{ gsub(/"/,""); print $3 }'`
 
-if [[ $JAVA_VERSION == 11 ]]; then
+if [[ $JAVA_VERSION == 11* ]]; then
   echo "Java version ok: $JAVA_VERSION"
 else
   echo "Java version is expected as 11 but $JAVA_VERSION. try:"
